bd96d41dae17cb099b633afb90c13733c4643543
Daria Sugak <sugakdariaa@gmail.com>
2025-10-16 13:04:20 +0300

    Added classes, a Course, and a Schedule, and implemented methods in the Student and Tutor classes.

--- Online_edu.py
+++ Online_edu.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import Dict, List
 
-
+#Абстрактный класс
 class Person(ABC):
     def __init__(self, first_name: str, last_name :str, age: int, phone: str,
                  email: str, user_id: int, role: str):
@@ -28,6 +28,7 @@ class Person(ABC):
         pass
 
 
+
 class Student(Person):
     def __init__(self, first_name: str, last_name :str, age: int, phone: str,
                  email: str, user_id: int, role: str, grade: int):
@@ -35,14 +36,19 @@ class Student(Person):
          self.grade = grade
          self.enrolled_courses: List[str] = []
 
-
     def display_info(self):
         print(f"Студент: {self.first_name} {self.last_name}")
         print(f"Класс: {self.grade}")
         print(f"Курсов записано: {len(self.enrolled_courses)}")
 
-    def choose_a_course(self):{}
-    def view_the_schedule(self): {}
+    def choose_a_course(self, course:'Course'):
+        course.add_student(self)
+        self.enrolled_courses.append(course)
+    def get_course(self):
+        return self.enrolled_courses
+
+    def view_the_schedule(self, schedule: 'Schedule'):
+        return schedule
 
 class Tutor(Person):
     def __init__(self, first_name: str, last_name :str, age: int, phone: str,
@@ -58,8 +64,52 @@ class Tutor(Person):
         print(f"Предмет: {self.subject}")
         print(f"Опыт: {self.experience} лет")
 
-    def create_course(self):{}
-    def view_the_schedule(self):{}
+
+    def create_course(self, name: str, subject: str, description: str,
+                 time: str, price: str, status: str) -> 'Course':
+        course = Course(name=name, tutor=self, subject=subject,  # передай все параметры
+                        description=description, time=time, price=price, status=status)
+        self.courses_taught.append(course)
+        return course
+
+    def view_the_schedule(self, schedule: 'Schedule'):
+        return schedule
+
+class Course():
+    def __init__(self, name: str, tutor: Tutor, subject: str, description: str,
+                 time: str, price: str, status: str):
+        self.name = name
+        self.tutor = tutor
+        self.subject = subject
+        self.description = description
+        self.time = time
+        self.price = price
+        self.status = status
+        self.students = []
+        self.lesson = []
+
+    def add_student(self,student: Student):
+        self.students.append(student)
+    def get_lessons(self):
+        return self.lesson
+    def get_students(self):
+        return self.students
+    def change_status(self,new_status: str):
+        self.status = new_status
+    def add_lessons(self, new_lesson: str):
+        self.lesson.append(new_lesson)
+
+class Schedule():
+    def __init__(self, student: Student, tutor: Tutor):
+        self.student = student
+        self.tutor = tutor
+        self.lessons = []
+
+    def add_lesson(self, new_lesson: str):
+        self.lessons.append(new_lesson)
+
+    def cancel_lesson(self, name_lesson):
+        self.lessons.remove(name_lesson)
 
 
 
